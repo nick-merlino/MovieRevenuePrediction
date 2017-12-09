f9005173c14f8b53a613cf6ddc3121b3b785734d
nick-merlino <nick@merlino.net>
2017-12-08 19:36:42 -0500

    Added keyword features

--- Preprocessing/generate_df_preproc.py
+++ Preprocessing/generate_df_preproc.py
@@ -3,41 +3,40 @@
 import pandas as pd # pip3 intsall pandas
 from pickle import dump
 import progressbar # pip3 install progressbar2
-from process_text import process
+from process_text import process, extract_key_phrases
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from pickle import dump
-from textrank import extract_key_phrases # pip3 install git+git://github.com/davidadamojr/TextRank.git
 from sklearn.pipeline import FeatureUnion
+from scipy.sparse import hstack
 
 # USER DEFINED
 num_output_classes = 3
 len_n_grams = 1
 
 # Show progress
-bar = progressbar.ProgressBar(max_value=5, widgets=[progressbar.Percentage(), progressbar.Bar(), ' [', progressbar.Timer(), ']'])
-bar.update(1)
+bar = progressbar.ProgressBar(value=0, max_value=5, widgets=[progressbar.Percentage(), progressbar.Bar(), ' [', progressbar.Timer(), ']'])
 
 # Read in movie metadata and plot summaries
 movie_headers =['Wikipedia movie ID', 'Freebase movie ID', 'Movie name','Movie release date','Movie box office revenue',
         'Movie runtime','Movie languages','Movie countries (Freebase ID:name tuples)','Movie genres']
+plot_headers = ['Wikipedia movie ID','Plot Summary']
 movie_metadata = pd.read_csv('MovieSummaries/movie.metadata.tsv',sep='\t',header=None,names=movie_headers,index_col=0)
-plot_summaries = pd.read_csv('MovieSummaries/plot_summaries.txt',sep='\t',index_col=0,names=['Wikipedia movie ID','Plot Summary'])
+plot_summaries = pd.read_csv('MovieSummaries/plot_summaries.txt',sep='\t',index_col=0,names=plot_headers)
 joined = plot_summaries.join(movie_metadata)
-plot_summaries_and_revenue = joined[['Plot Summary','Movie box office revenue']].dropna().iloc[:10]
+plot_summaries_and_revenue = joined[['Plot Summary','Movie box office revenue']].dropna()
 
 # Extract revenue classes and plot summaries
-revenue_classes, bins = pd.qcut(plot_summaries_and_revenue['Movie box office revenue'],num_output_classes, labels=list(range(1, num_output_classes+1)), retbins=True)
+revenue_classes, bins = pd.qcut(plot_summaries_and_revenue['Movie box office revenue'], num_output_classes, labels=list(range(1, num_output_classes+1)), retbins=True)
 summaries = plot_summaries_and_revenue['Plot Summary'].tolist()
 
 # Drop memory no longer needed
 del plot_summaries_and_revenue
+del movie_headers
+del movie_metadata
+del plot_headers
+del plot_summaries
 
-# Create summary overviews
-summary_overviews = []
-for s in summaries:
-    summary_overviews.append(' '.join(extract_key_phrases(s)))
-
-bar.update(2)
+bar.update(1)
 
 # Save revenue class labels
 bin_dict = dict()
@@ -45,17 +44,19 @@ for i in range(1, num_output_classes+1):
     bin_dict[i] = bins[i-1]
 
 # Vectorize each summary
-count_vect = CountVectorizer(ngram_range=(1, len_n_grams), strip_accents='unicode', decode_error='ignore', analyzer='word', tokenizer=process, stop_words='english')
-train_counts_summary = count_vect.fit_transform(summaries)
+count_vect_summary = CountVectorizer(ngram_range=(1, len_n_grams), strip_accents='unicode', decode_error='ignore', analyzer='word', tokenizer=process)
+train_counts_summary = count_vect_summary.fit_transform(summaries)
+
+bar.update(2)
 
 # Vectorize each summary overview
-count_vect = CountVectorizer(strip_accents='unicode', decode_error='ignore', analyzer='word', tokenizer=process, stop_words='english')
-train_counts_overview = count_vect.fit_transform(summaries)
+count_vect_keywords = CountVectorizer(strip_accents='unicode', decode_error='ignore', analyzer='word', tokenizer=extract_key_phrases)
+train_counts_keywords = count_vect_keywords.fit_transform(summaries)
 
 bar.update(3)
 
-# TODO: Combine vectors
-combined_features = FeatureUnion([('train_counts_summary', train_counts_summary), ('train_counts_overview', train_counts_overview)])
+# Combine vectors
+combined_features = hstack([train_counts_summary, train_counts_keywords])
 
 # Normalize vector
 tfidf_transformer = TfidfTransformer()
--- Preprocessing/process_text.py
+++ Preprocessing/process_text.py
@@ -4,6 +4,8 @@
 from nltk import word_tokenize, pos_tag
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords, wordnet
+from textrank import filter_for_tags, normalize, unique_everseen, build_graph # pip3 install git+git://github.com/davidadamojr/TextRank.git
+from networkx import pagerank
 
 # Define stopword list
 stop_list = stopwords.words('english')
@@ -30,4 +32,73 @@ def process(summary):
             wn_pos = get_wordnet_pos(tag)
             lemma = wnl.lemmatize(word,pos=wn_pos) if wn_pos is not None else wnl.lemmatize(word)
             results.append(lemma)
-    return list(results)
+    return results
+
+def extract_key_phrases(text):
+    # tokenize the text using nltk
+    word_tokens = word_tokenize(text)
+
+    # assign POS tags to the words in the text
+    tagged = pos_tag(word_tokens)
+    textlist = [x[0] for x in tagged]
+    textlist_lem = []
+    for i in range(len(textlist)):
+        word = textlist[i]
+        wn_pos = get_wordnet_pos(tagged[i][1])
+        lemma = wnl.lemmatize(word,pos=wn_pos) if wn_pos is not None else wnl.lemmatize(word)
+        textlist_lem.append(lemma)
+
+    tagged = filter_for_tags(tagged)
+    tagged = normalize(tagged)
+
+    unique_word_set = unique_everseen([x[0] for x in tagged])
+    word_set_list = list(unique_word_set)
+
+    # this will be used to determine adjacent words in order to construct
+    # keyphrases with two words
+
+    graph = build_graph(word_set_list)
+
+    # pageRank - initial value of 1.0, error tolerance of 0,0001,
+    calculated_page_rank = pagerank(graph, weight='weight')
+
+    # most important words in ascending order of importance
+    keyphrases = sorted(calculated_page_rank, key=calculated_page_rank.get,
+                        reverse=True)
+
+    # the number of keyphrases returned will be relative to the size of the
+    # text (a third of the number of vertices)
+    one_third = len(word_set_list) // 3
+    keyphrases = keyphrases[0:one_third + 1]
+
+    # take keyphrases with multiple words into consideration as done in the
+    # paper - if two words are adjacent in the text and are selected as
+    # keywords, join them together
+    modified_key_phrases = set([])
+    # keeps track of individual keywords that have been joined to form a
+    # keyphrase
+    dealt_with = set([])
+    i = 0
+    j = 1
+    while j < len(textlist):
+        first = textlist[i]
+        second = textlist[j]
+        if first in keyphrases and second in keyphrases:
+            keyphrase = textlist_lem[i] + ' ' + textlist_lem[j]
+            modified_key_phrases.add(keyphrase)
+            dealt_with.add(first)
+            dealt_with.add(second)
+        else:
+            if first in keyphrases and first not in dealt_with:
+                modified_key_phrases.add(textlist_lem[i])
+
+            # if this is the last word in the text, and it is a keyword, it
+            # definitely has no chance of being a keyphrase at this point
+            if j == len(textlist) - 1 and second in keyphrases and \
+                    second not in dealt_with:
+                modified_key_phrases.add(textlist_lem[j])
+
+        i = i + 1
+        j = j + 1
+
+    return modified_key_phrases
